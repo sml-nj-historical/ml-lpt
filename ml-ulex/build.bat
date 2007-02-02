21ea04541aa42222b3f298fd359e3fd9ae530c0c
Matthias Blume <blume@tti-c.org>
2007-02-02 21:08:38 +0000

    fixed bug in .bat script

--- ml-ulex/build.bat
+++ ml-ulex/build.bat
@@ -1,2 +1,3 @@
-..\..\bin\ml-build sources.cm Main.main ml-ulex
-..\..\bin\sml -m $smlnj/library-install.cm tool/ml-ulex-tool.cm ..\..\lib
+@ECHO off
+%COMSPEC% /C "..\..\bin\ml-build sources.cm Main.main ml-ulex"
+%COMSPEC% /C "..\..\bin\sml -m $smlnj/library-install.cm tool/ml-ulex-tool.cm ..\..\lib"
