b62b3b29485702aa2f228aa3e51892e6c274ce0e
Matthias Blume <blume@tti-c.org>
2007-05-04 14:56:58 +0000

    made build scripts independent of existence of tools

--- ml-antlr/build.bat
+++ ml-antlr/build.bat
@@ -1,3 +1,3 @@
 @ECHO off
-%COMSPEC% /C "..\..\bin\ml-build sources.cm Main.main ml-antlr"
+%COMSPEC% /C "..\..\bin\ml-build -DNO_ML_ULEX -DNO_ML_ANTLR sources.cm Main.main ml-antlr"
 %COMSPEC% /C "..\..\bin\sml -m $smlnj/library-install.cm tool/ml-antlr-tool.cm ..\..\lib"
--- ml-ulex/build.bat
+++ ml-ulex/build.bat
@@ -1,3 +1,3 @@
 @ECHO off
-%COMSPEC% /C "..\..\bin\ml-build sources.cm Main.main ml-ulex"
+%COMSPEC% /C "..\..\bin\ml-build -DNO_ML_ANTLR -DNO_ML_LEX -DNO_ML_YACC sources.cm Main.main ml-ulex"
 %COMSPEC% /C "..\..\bin\sml -m $smlnj/library-install.cm tool/ml-ulex-tool.cm ..\..\lib"
