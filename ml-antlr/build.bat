0155135ad139c2aba89bcd3679cd1a0482b14d99
Lars Bergstrom <larsberg@cs.uchicago.edu>
2007-10-22 20:43:15 +0000

    Enable building off of a parallels drive

--- ml-antlr/build.bat
+++ ml-antlr/build.bat
@@ -1,2 +1,2 @@
 @ECHO off
-%COMSPEC% /C "..\..\bin\ml-build -D NO_ML_ULEX -D NO_ML_ANTLR sources.cm Main.main ml-antlr"
+%COMSPEC% /C "..\..\bin\ml-build.bat -D NO_ML_ULEX -D NO_ML_ANTLR sources.cm Main.main ml-antlr"
